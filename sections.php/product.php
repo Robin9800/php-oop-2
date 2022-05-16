03912c3cf7ffbd031116625d80d3aa3b21b90e35
Robin9800 <robin.costanzo@gmail.com>
2022-05-16 22:59:22 +0200

    product content

---
+++ sections.php/product.php
@@ -0,0 +1,13 @@
+<?php
+
+    class Product {
+        private $name;
+        private $price;
+
+        function __construct($_nameItem,$_price) {
+            $this->nameItem = $_nameItem;
+            $this->_price = $_price;
+        }
+    }
+
+?>
